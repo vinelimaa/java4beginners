be0e7005232e4419f1fba41ec49f286158bb901a
vinelimaa <vinicius.damas@hotmail.com>
2023-02-08 15:21:13 -0300

    multiplication table example

---
+++ FirstJavaProject/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -0,0 +1,13 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+//		table.print(10);
+//		table.print(5, 10, 20);
+	}
+
+}
